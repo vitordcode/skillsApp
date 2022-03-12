6edfbf26adb05695992d7a41d31c28824b1de943
Vitor de Paula <vitordcode@gmail.com>
2022-03-12 13:58:15 -0300

    Using FlatList in Component SkillCard

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   TextInput,
   Platform,
+  FlatList,
 } from 'react-native'
 
 import { Button } from '../components/Button'
@@ -29,13 +30,17 @@ export function Home() {
         onChangeText={setNewSkill}
       />
 
-      <Button onPress={handleAddNewSkill}/>
+      <Button onPress={handleAddNewSkill} />
 
       <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
 
-      {mySkills.map(skill => (
-        <SkillCard skill={skill}/>
-      ))}
+      <FlatList 
+        data={mySkills}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <SkillCard key={item} skill={item} />
+        )}
+      />
     </View>
   )
 }
