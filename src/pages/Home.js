affac6e46414e00b7b2927d7d95bb17318549f91
Vitor de Paula <vitordcode@gmail.com>
2022-03-12 13:48:02 -0300

    First Commit

---
+++ src/pages/Home.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Platform,
+} from 'react-native'
+
+import { Button } from '../components/Button'
+import { SkillCard } from '../components/SkillCard'
+
+export function Home() {
+  const [newSkill, setNewSkill] = useState('')
+  const [mySkills, setMySkills] = useState([])
+
+  function handleAddNewSkill() {
+    setMySkills(oldState => [...oldState, newSkill])
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Vitor</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New skill"
+        placeholderTextColor="#555"
+        onChangeText={setNewSkill}
+      />
+
+      <Button onPress={handleAddNewSkill}/>
+
+      <Text style={[styles.title, { marginVertical: 50 }]}>My Skills</Text>
+
+      {mySkills.map(skill => (
+        <SkillCard skill={skill}/>
+      ))}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: '#1f1e25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 5,
+  },
+})
